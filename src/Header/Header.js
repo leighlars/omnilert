3a45a6a3fee7dde11f4156984d7b66f4eda88ec4
Leigh Larson <leighlarson4@gmail.com>
2020-10-19 17:43:24 -0600

    Add spacing between text and form in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,7 +5,7 @@ import Search from '../Search/Search'
 const Header = () => {
   return(
     <header>
-      <h1>Get</h1> <h1 className='giphy-header'>Giphy</h1><h1>Wit It</h1>
+      <div className='header-text'><h1>Get</h1> <h1 className='giphy-header'>Giphy</h1><h1>Wit It</h1></div>
       <Search />
     </header>
   )
