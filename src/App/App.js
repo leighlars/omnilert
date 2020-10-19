49ca0823dddc6b874e8f2e1bd8a4405abd9b0d7b
Leigh Larson <leighlarson4@gmail.com>
2020-10-19 16:31:57 -0600

    Add Header component and styling

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Header from '../Header/Header'
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Get Giphy Wit It</h1>
+    </header>
+  )
+}
+
+export default Header
